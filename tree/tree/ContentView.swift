9ca054411ba02b62c2be7fcb94cd3ec64cb8349c
NikolaiMadlener <nikolai@madlener.io>
2023-11-18 12:06:05 +0100

    include coaty

---
+++ tree/tree/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  tree
+//
+//  Created by Nikolai Madlener on 18.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tree/tree/treeApp.swift
@@ -0,0 +1,17 @@
+//
+//  treeApp.swift
+//  tree
+//
+//  Created by Nikolai Madlener on 18.11.23.
+//
+
+import SwiftUI
+
+@main
+struct treeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
